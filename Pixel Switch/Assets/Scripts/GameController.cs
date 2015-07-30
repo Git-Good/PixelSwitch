150c9a705004fafcd7f7b8953329eb03b63296cd
krytical <johnnie-l@Live.ca>
2015-07-30 11:12:24 -0700

    Starting pose switching

---
+++ Pixel Switch/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PoseOne() {
+
+	}
+
+	public void PoseTwo(){
+
+	}
+
+	public void PoseThree(){
+
+	}
+
+	public void PoseFour(){
+	
+	}
+}
