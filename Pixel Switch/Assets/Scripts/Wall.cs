bf080b29ccfeed860f9cb5425f8e98d69b6ed885
krytical <johnnie-l@Live.ca>
2015-07-30 17:28:57 -0700

    Added Placeholder objects

---
+++ Pixel Switch/Assets/Scripts/Wall.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	public string charPos;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveWall(){
+
+
+	}
+}
--- Pixel Switch/Assets/Scripts/WallSpawner.cs
+++ Pixel Switch/Assets/Scripts/WallSpawner.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class WallSpawner : MonoBehaviour {
 
+	public GameObject[] wallList;
+
 	// Use this for initialization
 	void Start () {
-	
+		SpawnNewWall ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public void SpawnNewWall(){
+		int i = Random.Range (0, wallList.Length);
+		Instantiate (wallList [i], transform.position, Quaternion.identity);
 	}
 }
