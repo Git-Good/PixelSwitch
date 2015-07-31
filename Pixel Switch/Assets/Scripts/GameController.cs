0b2fe76aa3e86044afd2a7f9286c73dcb905291c
nivek535 <chern117@hotmail.com>
2015-07-30 21:57:23 -0700

    chern's first commit, john wall and gamecontroller
    put some shit in update, gave john and wall fields.
    gamecontroller line 18 and 23 have errors because obj not instantiated

--- Pixel Switch/Assets/Scripts/GameController.cs
+++ Pixel Switch/Assets/Scripts/GameController.cs
@@ -3,29 +3,50 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	John j;
+	Wall w;
+	bool checker = true;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
-	
+		Check ();
+		w.MoveWall ();
 	}
 
-	public void PoseOne() {
+	public void Check(){
+		if (w.xcoord == j.xcoord) { // check only if wall reaches john
+			if (w.pose == j.pose) { // check if wall john fits through wall
+				checker = true;
+			} else {
+				checker = false; //end game
+			}
+		}
+	}
 
+
+	public void PoseOne() {
+		j.pose = 1;
+		//j.im = po1.bmp;
 	}
 
 	public void PoseTwo(){
-
+		j.pose = 2;
+		//j.im = po2.bmp;
 	}
 
 	public void PoseThree(){
-
+		j.pose = 3;
+		//j.im = po3.bmp;
 	}
 
 	public void PoseFour(){
-	
+		j.pose = 4;
+		//j.im = po4.bmp;
 	}
 }
--- Pixel Switch/Assets/Scripts/John.cs
+++ Pixel Switch/Assets/Scripts/John.cs
@@ -1,23 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Wall : MonoBehaviour {
+public class John : MonoBehaviour {
 
-	public int xcoord;
-	public int pose;
 	// Use this for initialization
+	public int pose;
+	public int xcoord;
+	// pixel art image
+	// image im;??
 	void Start () {
-
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// do updates in GameController
-	}
-
-	public void MoveWall(){
-		// johnnie said push....
-
 	}
 }
